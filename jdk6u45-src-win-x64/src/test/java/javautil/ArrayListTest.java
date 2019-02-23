341104fd1937224a6e3b9a977dc302d11b5b9a38
蔡盛智 <caishengzhi@le.com>
2019-02-23 16:35:04 +0800

    Arrays.copyOf和System.arraycopy两个方法测试

--- jdk6u45-src-win-x64/src/test/java/javautil/ArrayListTest.java
+++ jdk6u45-src-win-x64/src/test/java/javautil/ArrayListTest.java
@@ -25,8 +25,11 @@ public class ArrayListTest {
 
         int[] arr = new int[5];
         System.out.println("oldLength:" + arr.length);
-        Arrays.copyOf(arr, 6);
-        System.out.println("newLength:" + arr.length);
+        /**
+         * 将arr改为长度为7的newArr，newArr包含arr的所有元素
+         */
+        int[] newArr = Arrays.copyOf(arr, 7);
+        System.out.println("newLength:" + newArr.length);
 
     }
 
@@ -34,7 +37,10 @@ public class ArrayListTest {
 
         int[] arr = new int[]{1,2,3,7,8,9};
         int[] newArr = new int[3];
-        System.arraycopy(arr, 0, newArr, 0, 3);
+        /**
+         * 从源数组arr的下标0开始，复制2个数据，到新数组newArr的下标1(包含1)往后的两个位置
+         */
+        System.arraycopy(arr, 0, newArr, 1, 2);
         System.out.println("originArr:");
         this.print(arr);
         System.out.println("newArr:");
